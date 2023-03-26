ba60e0fe6f4c67b418b582ae33fa11eb2f8f61a5
Vlad Tudose <50240588+vaisi@users.noreply.github.com>
2023-03-26 15:38:59 +0000

    executing a pay transaction of 1ALGO, finally managed to get a signedTxnByteArray using TextEncoder

--- public/scripts/test_transaction.js
+++ public/scripts/test_transaction.js
@@ -9,55 +9,41 @@ async function sendTransaction() {
   try {
     const accounts = await AlgoSigner.accounts({ ledger: 'TestNet' });
     const account = accounts[0];
-    const params = await algodClient.getTransactionParams().do();
     const sender = account.address;
     const recipient = "R42P25CWZ23WITFSQHGZTYCACIUIZJVLQBKW57MB727QFP723NRBOJD5AM";
     const amount = 1 * MICROALGO;
-    const note = new TextEncoder().encode('Hello, Algorand!');
 
-    const encoder = new TextEncoder();
-
-    const suggestedTxParams = {
-      "type": "pay",
-      "fee": params.fee,
-      "firstRound": params.firstRound,
-      "lastRound": params.lastRound,
-      "genesisID": params.genesisID,
-      "genesisHash": params.genesisHash,
-    };
 
     log(`Trying to execute transaction from ${sender} to ${recipient} ...`);
 
     // appIndex: +document.getElementById('app-index').value,
-    const txn = algosdk.makeApplicationNoOpTxnFromObject({
+    const suggestedTxParams = await algodClient.getTransactionParams().do();
+    const _txn = await algosdk.makePaymentTxnWithSuggestedParamsFromObject({
       from: sender,
       to: recipient,
       amount: amount,
-      type: "pay",    
+      type: "pay",
       suggestedParams: { ...suggestedTxParams }
     });
 
     // Use the AlgoSigner encoding library to make the transactions base64
-    let txn_b64 = AlgoSigner.encoding.msgpackToBase64(txn.toByte());
-    const signedTxn = await AlgoSigner.signTxn([{ txn: txn_b64 }]);
-    
-    /*
-    AlgoSigner.signTxn([{ txn: txn_b64 }]).then((d) => {
-      signedTxn = d;
-      log("AlgoSigner signed transaction " + signedTxn);      
-    }).catch((e) => { log("Error: " + e); });
-    */
+    log("1");
+    let txn_b64 = AlgoSigner.encoding.msgpackToBase64(_txn.toByte());    
+    let signedTxn = await AlgoSigner.signTxn([{ txn: txn_b64 }]);
+    log("3 signedTxn " + signedTxn);
 
-    log("signed transaction " + signedTxn);
-    if(signedTxn){      
-      log('Transaction executed successfully:', signedTxn);
-
-      for (const key in signedTxn) {
-        log(`${key}:`, signedTxn[key]);
-      log("checking if amout was received by recepient...");
-      }
-    }
+    const encoder = new TextEncoder();
+    const signedTxnByteArray = encoder.encode(signedTxn);
     
+    log("3 signedTxn " + signedTxnByteArray);
+    const { txId } = await algodClient.sendRawTransaction(signedTxnByteArray).do();
+    log("4");
+    const result = await algosdk.waitForConfirmation(algodClient, txId, 4);
+
+    log(result);
+    log(`Transaction Information: ${result.txn}`);
+    //log(`Decoded Note: ${Buffer.from(result.txn.txn.note).toString()}`);
+
   } catch (err) {
     log('Error:', err);
   }
